bc4131d2d50fc3fc261583c9b66c7872ea89251f
Tural Sadigov <turalsadikov@gmail.com>
2023-07-11 07:44:07 -0700

    manifest

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 def is_prime(num):
     if num < 2:
         return False
@@ -8,15 +9,17 @@ def is_prime(num):
             return False
     return True
 
+
 def main():
     st.title("Prime Number Checker")
     number = st.number_input("Enter a number:")
-    
+
     if st.button("Check"):
         if is_prime(number):
             st.success(f"{number} is a prime number!")
         else:
             st.error(f"{number} is not a prime number.")
+
 
 if __name__ == "__main__":
     main()
